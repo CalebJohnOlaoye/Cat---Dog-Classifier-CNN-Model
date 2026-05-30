88d8aa0580d37bf813e3f30ad3ccfe71153ed172
Caleb-John <calebjohnolaoye7@gmail.com>
2026-05-30 10:02:49 +0100

    Saved the model to cache for the web aapp on streamlit

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from keras.models import load_model
 import numpy as np 
 from keras.preprocessing import image
 
-model = load_model("cat_dog_classifier.keras")
+
+@st.cache_resource
+def load_my_model():
+    # looks for the file in the same directory as the app.py file
+    return load_model("cat_dog_classifier.keras")
+
+model = load_my_model()
 
 st.title("Cat vs Dog Classifier")
 
@@ -12,8 +18,8 @@ upload = st.file_uploader(
     "Upload an image",
     type=["jpg", "png", "jpeg"]
 )
-if upload == None:
-  st.write("""
+if upload is None:
+  st.info("""
     ## Please Upload an IMAGE of your favourite pet
   """)
 else:   
@@ -22,11 +28,15 @@ else:
     test_image = test_image / 255.0
     test_image = np.expand_dims(test_image, axis = 0)
     result = model.predict(test_image)
-    if result[0][0] >= 0.5:
-        prediction = 'DOG'
-    else:
-        prediction = 'CAT'
-
     st.write(f"---{result[0][0]}")
     st.subheader('Prediction Result:')
-    st.success(f"The Model predicts this is a **{prediction}**!")
+    if result[0][0] >= 0.5:
+        prediction = 'DOG'
+        st.success(f"The Model predicts this is a **{prediction}** Confidence : {result[0][0]:.2f}!")
+    else:
+        prediction = 'CAT'
+        st.success(f"The Model predicts this is a **{prediction}** Confidence : {result[0][0]:.2f}!")
+
+    
+    
+   
